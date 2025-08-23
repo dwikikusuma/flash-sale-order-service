7535465b34b2cfd3d17c2f66819cb49fc7efc8ad
dwikikusuma <kusumabriantono@gmail.com>
2025-08-23 15:25:02 +0700

    add configuration management: implement config loading and structure for application settings

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,41 @@
 package main
 
 import (
-	echojwt "github.com/labstack/echo-jwt/v4"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	infrastructure2 "order-service/infrastructure/log"
+	"order-service/config"
+	infrastructure "order-service/infrastructure/log"
 	"order-service/internal/api"
 	"order-service/internal/repository"
 	"order-service/internal/service"
-	middleware2 "order-service/middleware"
+	reqMiddleware "order-service/middleware"
 	"order-service/routes"
 	"time"
+
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	infrastructure2.InitLogger()
+
+	infrastructure.InitLogger()
+
+	appConfig := config.LoadConfig(
+		config.WithConfigFolder([]string{"./files/config"}),
+		config.WithConfigFile("./files/config"),
+		config.WithConfigType("yaml"),
+	)
 
 	orderRepo := repository.NewOrderRepository()
-	orderService := service.NewOrderService(orderRepo, "order-service/internal/config", "order-service/internal/config/config.yaml")
+	orderService := service.NewOrderService(orderRepo, appConfig.Services.Product, appConfig.Services.Pricing)
 	orderHandler := api.NewOrderHandler(orderService)
 
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Use(middleware.RateLimiterWithConfig(middleware2.GetRateLimiter()))
+	e.Use(middleware.RateLimiterWithConfig(reqMiddleware.GetRateLimiter()))
 	e.Use(middleware.ContextTimeout(15 * time.Second))
-	e.Use(echojwt.JWT("secrete"))
+	e.Use(echojwt.JWT(appConfig.Secret.JWTSecret))
 
 	routes.SetupRoutes(e, orderHandler)
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":" + appConfig.App.Port))
 }
---
+++ config/type.go
@@ -0,0 +1,36 @@
+package config
+
+type Config struct {
+	App      App           `mapstructure:"app" validate:"required"`
+	DB       DB            `mapstructure:"db" validate:"required"`
+	Redis    Redis         `mapstructure:"redis" validate:"required"`
+	Secret   SecreteConfig `mapstructure:"secret" validate:"required"`
+	Services Services      `mapstructure:"services" validate:"required"`
+}
+
+type App struct {
+	Port string `mapstructure:"port" validate:"required"`
+}
+
+type DB struct {
+	Host     string `mapstructure:"host" validate:"required"`
+	Port     string `mapstructure:"port" validate:"required"`
+	User     string `mapstructure:"user" validate:"required"`
+	Password string `mapstructure:"password" validate:"required"`
+	Name     string `mapstructure:"name" validate:"required"`
+}
+
+type SecreteConfig struct {
+	JWTSecret string `mapstructure:"jwt_secret" validate:"required"`
+}
+
+type Redis struct {
+	Host     string `mapstructure:"host" validate:"required"`
+	Port     string `mapstructure:"port" validate:"required"`
+	Password string `mapstructure:"password"`
+}
+
+type Services struct {
+	Product string `mapstructure:"product" validate:"required"`
+	Pricing string `mapstructure:"pricing" validate:"required"`
+}
